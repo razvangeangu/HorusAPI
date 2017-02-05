9a077a744d57587a6f28dd360b6a7fdd68050ef5
Razvan-Gabriel Geangu <razvan.geangu@gmail.com>
2017-02-05 05:19:52 +0000

    small changes

--- server/app.js
+++ server/app.js
@@ -4,9 +4,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-var cors = require('cors');
-
 var logStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
+var cors = require('cors');
 
 cors(app);
 
@@ -22,14 +21,14 @@ io.on('connection', function (socket) {
     console.log(username + ' connected from ip:  ' + socket.handshake.address);
 
     socket.on('client:message', function (data) {
-        console.log(data);
+//        console.log(data);
 
         // logStream.write(socket.handshake.address + ": " + data.message + "\n");
         // fs.writeFile("photo.jpg", new Buffer(data, "base64"), function(err) {});
 
         // message received from client, now broadcast it to everyone else
-	io.emit('server:message', 'Asif');
-    });
+	io.emit('server:message',['Apple for life', '30', '20']);
+    })
 
     socket.on('disconnect', function () {
         console.log(username + ' disconnected');
